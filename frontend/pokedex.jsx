5e750cc48805687874862f32c9f9e7d8e5b7e10f
PrestonPhelan <preston.phelan@gmail.com>
2017-01-04 14:48:38 -0800

    implement requestAllPokemon function combining request and success handler into one callable dispatch function

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import * as APIUtil from './util/api_util';
-import { receiveAllPokemon } from './actions/pokemon_actions';
+import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
 
 import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   window.store = store;
+  window.requestAllPokemon = requestAllPokemon;
   window.receiveAllPokemon = receiveAllPokemon;
   window.fetchAllPokemon = APIUtil.fetchAllPokemon;
   const rootEl = document.getElementById('root');
