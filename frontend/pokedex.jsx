7ba835db81902a929b26babe44fefac607f1184f
PrestonPhelan <preston.phelan@gmail.com>
2017-01-04 14:25:16 -0800

    Implement basic reducer, store, basic action, ajax request for pokemon index

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as APIUtil from './util/api_util';
+import { receiveAllPokemon } from './actions/pokemon_actions';
+
+import configureStore from './store/store';
+
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  window.store = store;
+  window.receiveAllPokemon = receiveAllPokemon;
+  window.fetchAllPokemon = APIUtil.fetchAllPokemon;
   const rootEl = document.getElementById('root');
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
